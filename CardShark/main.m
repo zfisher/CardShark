5e0d3ae399e500b33482da45016bfc8f3675f69a
Suggy <asugaya@mit.edu>
2010-05-20 02:34:33 -0400

    Added CardShark

---
+++ CardShark/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CardShark
+//
+//  Created by Andrew Sugaya on 5/20/10.
+//  Copyright Mythos 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
